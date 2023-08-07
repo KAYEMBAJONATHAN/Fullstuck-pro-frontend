b902f657a9c4145f77d25544d3937b2dfecd3379
kayemba <kayembejonathan5@gmail.com>
2023-08-07 22:18:52 +0200

    fix the error

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,7 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
 import cartReducer from '../redux/slices/cartSlice';
 import productReducer from '../redux/slices/productSlice';
-import orderReducer from '../redux/slices/orderSlice';e
+import orderReducer from '../redux/slices/orderSlice';
 import userReducer from '../redux/slices/userSlice';
 
 const store = configureStore({
